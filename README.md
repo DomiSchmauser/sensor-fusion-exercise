2b652f49915b26d24fb198ba79168cd69e1814f4
Dominik Schmauser <dominik.schmauser@helsing.ai>
2025-02-11 21:58:43 +0100

    Cleanup and rm solution

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # Sensor Fusion Exercise
+Welcome to the EDTH sensor fusion exercise in which you will:
+- Set up an array of two camera sensors that are 50 meters apart from each other and observe a 2D map (flat surface)
+- Derive an object recognition algorithm to detect and localise a tank and a car in the map
+- Implement the core logic for fusing identified objects into a common operational picture
+
+### Setting
+We use a local right-handed cartesian coordinate system for which `x` points north and `y` points east. Each camera is facing towards the objects which is configured by the
+cameras bearing (or azimuth) angle with 0° bearing = facing parallel north and 90° bearing = facing parallel east.
+
+<img src='src/sensor_fusion_exercise/resources/setting.png' width=25% height=25%/>
+
+### Modules
+This exercise comprises of 3 modules:
+- `sensor.py` defining the camera logic for streaming images
+- `object_recognition.py` defining the algorithm to process camera images, detect objects and generate a unique object identification
+- `fusion_station.py` defining the central system to fuse identified objects into a shared representation
+
+Main entry point with a step-by-step guide: `main.ipynb`
+
+### Setup
+Install required packages via [poetry](https://python-poetry.org/docs/) by executing from the source directory:
+```
+# Ensure Python >= 3.10 is installed, otherwise you can download with
+pyenv install 3.10
+pyenv global 3.10
+
+# Install the virtual environment
+poetry shell
+poetry install
+```# Sensor Fusion Exercise
--- src/sensor_fusion_exercise/main_IDE.py
+++ src/sensor_fusion_exercise/main_IDE.py
@@ -17,12 +17,7 @@ def monocular_distance_from_object_prior(identified_object: IdentifiedObject, fr
     Calculates the distance based on an object size prior and the respective bounding box size.
     Returns a distance estimate from the camera to the target object in meters.
     """
-    image_width = frame.image.shape[1]  # Retrieve width pixels dimension from image in the format H x W x C
-    bbox_width_degree = identified_object.bounding_box.width / image_width * frame.fov_horizontal
-    distance = Meters(
-        OBJECT_PRIORS_WIDTH[identified_object.class_name] / (2.0 * np.tan(np.deg2rad(bbox_width_degree) / 2.0))
-    )
-    return distance
+    pass
 
 
 def match_and_fuse_identified_objects(
@@ -36,41 +31,9 @@ def match_and_fuse_identified_objects(
     """
 
     def distance_metric(object1: IdentifiedObject, object2: IdentifiedObject) -> Meters:
-        # Euclidian distance
-        euclidian_distance = math.sqrt(
-            (object2.object_location_north_east.north - object1.object_location_north_east.north) ** 2
-            + (object2.object_location_north_east.east - object1.object_location_north_east.east) ** 2
-        )
-        return Meters(euclidian_distance)
+        pass
 
-    # Generate cost matrix
-    cost_matrix = np.zeros((2, 2))
-    sensor_1_objects = sensor_object_mapping[1]
-    sensor_2_objects = sensor_object_mapping[2]
-    # rows = sensor 1, columns = sensor2
-    for row_idx, o1 in enumerate(sensor_1_objects):
-        for col_idx, o2 in enumerate(sensor_2_objects):
-            cost_matrix[row_idx, col_idx] = distance_metric(o1, o2)
-
-    # Bipartite graph matching
-    row_idx, col_idx = linear_sum_assignment(cost_matrix)
-    fused_objects = []
-    for row, col in zip(row_idx, col_idx, strict=True):  # type: ignore[call-overload]
-        s1_object = sensor_1_objects[row]
-        s2_object = sensor_2_objects[col]
-        logger.info(
-            f"Fused sensor 1 identified object at location {s1_object.object_location_north_east} and "
-            f"sensor 2 identified object at location {s2_object.object_location_north_east}"
-        )
-
-        # Re-calculate the fused location as the average from both perspectives
-        fused_north = (s1_object.object_location_north_east.north + s2_object.object_location_north_east.north) / 2.0
-        fused_east = (s1_object.object_location_north_east.east + s2_object.object_location_north_east.east) / 2.0
-        fused_location = LocationNE(north=fused_north, east=fused_east)
-        s1_object.object_location_north_east = fused_location
-        fused_objects.append(s1_object)
-
-    return fused_objects
+    pass
 
 
 if __name__ == "__main__":
